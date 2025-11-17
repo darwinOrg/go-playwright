d4c79e9a5598b78d90cfe8e9428228fc25c8dd47
陈彪 <chenbiao@globalpand.cn>
2025-11-17 18:27:24 +0800

    up

--- chrome_test.go
+++ chrome_test.go
@@ -1,13 +1,14 @@
 package extpw
 
 import (
+	"fmt"
 	"testing"
 	"time"
 )
 
 func TestStartChrome(t *testing.T) {
 	debugPort := 9222
-	_, _, _ = StartChrome(debugPort)
+	chromeCmd, _, _ := StartChrome(debugPort)
 	time.Sleep(2 * time.Second)
-	//ShutdownChrome(chromeCmd, fmt.Sprintf("http://localhost:%d", debugPort))
+	ShutdownChrome(chromeCmd, fmt.Sprintf("http://localhost:%d", debugPort))
 }
